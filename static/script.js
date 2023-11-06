145f1ffa34120e0aa17908b8a3b2d31f196c35e3
LiyanAlJallad <laljalla@uwo.ca>
2023-11-06 15:31:55 -0500

    3rd script

--- static/script.js
+++ static/script.js
@@ -154,3 +154,207 @@ async function populateListsDropdown() {
         }
     });
 }
+
+async function displayListDetails() {
+    const listSelect = document.getElementById('listsDropdown');
+    const listName = listSelect.value;
+  
+    if (!listName) {
+      displayError('Please select a list to display its details.');
+      return;
+    }
+  
+    try {
+      // Fetch the detailed information of superheroes in the selected list
+      const response = await fetch(`/api/superhero_lists/${encodeURIComponent(listName)}/info`);
+      if (!response.ok) {
+        throw new Error("There was a problem with the fetch operation.");
+      }
+      const superheroes = await response.json();
+      superheroes.sort((a, b) => a.name.localeCompare(b.name)); // Add this line to sort the data
+
+  
+      // Clear previous results
+      const resultsDiv = document.getElementById('results');
+      resultsDiv.innerHTML = '';
+  
+      // Display each superhero's details and powers
+      superheroes.forEach(superhero => {
+        const card = document.createElement('div');
+        card.className = 'superhero-card';
+  
+        const name = document.createElement('h3');
+        name.innerText = superhero.name;
+  
+        const info = document.createElement('p');
+        info.innerText = `Race: ${superhero.Race || 'N/A'}\nPublisher: ${superhero.Publisher || 'N/A'}`;
+  
+        const powersList = document.createElement('ul');
+        Object.entries(superhero.powers).forEach(([power, value]) => {
+          if (value === "True") {
+            const powerItem = document.createElement('li');
+            powerItem.innerText = power;
+            powersList.appendChild(powerItem);
+          }
+        });
+  
+        card.appendChild(name);
+        card.appendChild(info);
+        card.appendChild(powersList);
+  
+        resultsDiv.appendChild(card);
+      });
+    } catch (error) {
+      console.error("Error during fetching list details:", error);
+      displayError(error.message);
+    }
+}
+
+async function populateSuperheroNamesDropdown() {
+    const superheroNamesDropdown = document.getElementById('superheroNamesDropdown');
+    const response = await fetch('/api/superhero_info/allNames');
+    const superheroNames = await response.json();
+
+    // Clear existing options
+    superheroNamesDropdown.innerHTML = '<option value="">Select a superhero</option>';
+    
+    // Populate dropdown with superhero names
+    superheroNames.forEach(name => {
+        const option = document.createElement('option');
+        option.value = name;
+        option.innerText = name;
+        superheroNamesDropdown.appendChild(option);
+    });
+}
+
+async function populateSuperheroNamesDropdown() {
+    const superheroNamesSelect = document.getElementById('superheroNamesSelect');
+    try {
+        const response = await fetch('/api/superhero_info/allNames');
+        if (!response.ok) {
+            throw new Error(`HTTP error! status: ${response.status}`);
+        }
+        const superheroNames = await response.json();
+
+        // Clear existing options
+        superheroNamesSelect.innerHTML = '';
+
+        // Populate select with superhero names
+        superheroNames.forEach(name => {
+            const option = document.createElement('option');
+            option.value = name;
+            option.textContent = name; // Display text for the option
+            superheroNamesSelect.appendChild(option);
+        });
+    } catch (error) {
+        console.error('Failed to populate superhero names:', error);
+        // Handle the error accordingly in your UI
+    }
+}
+
+async function addSuperheroesToList() {
+    // Get the selected superhero names
+    const superheroNamesSelect = document.getElementById('superheroNamesSelect');
+    const selectedOptions = superheroNamesSelect.selectedOptions;
+    const selectedNames = Array.from(selectedOptions).map(option => option.value);
+
+    // Get the selected list name
+    const listsDropdown = document.getElementById('listsDropdown');
+    const listName = listsDropdown.value;
+
+    if (!listName) {
+        displayError('Please select a list.');
+        return;
+    }
+
+    try {
+        // Get IDs for the selected superhero names
+        const idsResponse = await fetch('/api/superhero_info/getIDs', {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json',
+            },
+            body: JSON.stringify({ names: selectedNames })
+        });
+
+        if (!idsResponse.ok) {
+            throw new Error("Error fetching superhero IDs");
+        }
+
+        const ids = await idsResponse.json();
+
+        // Update the selected list with the new IDs
+        const listResponse = await fetch(`/api/superhero_lists/${encodeURIComponent(listName)}`, {
+            method: 'PUT',
+            headers: {
+                'Content-Type': 'application/json',
+            },
+            body: JSON.stringify({ ID: ids })
+        });
+
+        if (!listResponse.ok) {
+            const errorText = await listResponse.text();
+            throw new Error(errorText || "There was a problem updating the list.");
+        }
+
+        displaySuccess(`Superheroes added to "${listName}" successfully!`);
+        // Update the UI accordingly
+
+    } catch (error) {
+        console.error("Error during adding superheroes to list:", error);
+        displayError(error.message);
+    }
+}
+
+function displaySuccess(message) {
+    const resultsDiv = document.getElementById('results');
+    // Clear previous content
+    resultsDiv.innerHTML = '';
+    
+    // Create a div to show the success message
+    const successDiv = document.createElement('div');
+    successDiv.className = "success";
+    successDiv.innerText = message;
+    
+    // Append the success div to resultsDiv
+    resultsDiv.appendChild(successDiv);
+
+    // Optionally, you can make the success message disappear after a few seconds
+    setTimeout(() => {
+        successDiv.remove();
+    }, 5000); // Removes the success message after 5 seconds
+}
+
+async function deleteList() {
+    const listSelect = document.getElementById('listsDropdown');
+    const listName = listSelect.value;
+
+    if (!listName) {
+        displayError('Please select a list to delete.');
+        return;
+    }
+
+    try {
+        const response = await fetch(`/api/superhero_lists/${encodeURIComponent(listName)}`, {
+            method: 'DELETE',
+        });
+
+        if (!response.ok) {
+            throw new Error("There was a problem deleting the list.");
+        }
+
+        // Remove the deleted list from the dropdown
+        listSelect.remove(listSelect.selectedIndex);
+
+        displaySuccess(`List "${listName}" deleted successfully!`);
+    } catch (error) {
+        console.error("Error during deleting the list:", error);
+        displayError(error.message);
+    }
+}
+
+document.addEventListener('DOMContentLoaded', function() {
+    populatePowerDropdown();
+    populateListsDropdown();
+    populateSuperheroNamesDropdown(); // Call this function to populate the dropdown
+});
